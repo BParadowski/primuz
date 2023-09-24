57b874f9ed1154162a687c14351ddb0adf1a0c9c
BParadowski <bartoszparadowski01@gmail.com>
2023-09-24 20:41:57 +0200

    Try to get onesignal to work

--- lib/onesignal.ts
+++ lib/onesignal.ts
@@ -3,9 +3,6 @@ import OneSignal from "react-onesignal";
 export default async function runOneSignal() {
   await OneSignal.init({
     appId: "a048564d-ce47-464a-be18-815088ddc93b",
-    safari_web_id: "web.onesignal.auto.1b5e3a9a-fd8d-4cbc-b150-cc0a98b0f0fe",
-    serviceWorkerParam: { scope: "/notifications/onesignal/" },
-    serviceWorkerPath: "/notifications/onesignal/OneSignalSDKWorker.js",
   });
 
   await OneSignal.Slidedown.promptPush();
