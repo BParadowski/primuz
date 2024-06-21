bb63d6cdde4f937670060f25a97a88ca3253289c
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2024-06-21 15:14:42 +0200

    Remove usage of a view on projects page

--- app/(logged-in)/projekty/page.tsx
+++ app/(logged-in)/projekty/page.tsx
@@ -7,7 +7,19 @@ export const dynamic = "force-dynamic";
 
 export default async function Projects() {
   const supabase = createServerComponentClient<Database>({ cookies });
-  const { data } = await supabase.from("projects_summary").select();
+  const userId = (await supabase.auth.getSession()).data.session?.user.id;
+  if (!userId) return;
+
+  const theDayBeforeYesterday = new Date(
+    new Date().setDate(new Date().getDate() - 2),
+  ).toISOString();
+
+  const { data } = await supabase
+    .from("availability")
+    .select("status, message, projects!inner(id, date, name, location)")
+    .eq("user_id", userId)
+    .gte("projects.date", theDayBeforeYesterday)
+    .order("projects(date)", { ascending: true });
 
   return (
     <main className="mt-[--header-height] grid md:mt-0">
@@ -16,9 +28,17 @@ export default async function Projects() {
           <h1 className="py-2 text-center text-2xl font-bold sm:py-4">
             Projekty
           </h1>
-          {data?.map((projectData) => (
-            <ProjectCard projectData={projectData} key={projectData.id} />
-          ))}
+          {data?.map((projectData) => {
+            console.log(projectData);
+            const { status, message, projects: project } = projectData;
+
+            return (
+              <ProjectCard
+                projectData={{ status, message, ...project }}
+                key={project.id}
+              />
+            );
+          })}
         </div>
       </div>
     </main>
--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,7 @@ import type { Database } from "@/lib/supabase.ts";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
-  const user = await supabase.auth
-    .getSession()
-    .then(({ data: { session } }) => session?.user);
+  const user = await supabase.auth.getUser().then(({ data: { user } }) => user);
 
   if (user && req.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/projekty", req.url));
